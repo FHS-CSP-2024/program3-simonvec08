fdb931200d64c6895af717673592ec28a3421b9d
simonvec08 <simonvec08@gmail.com>
2024-09-08 06:28:19 +0000

    final

--- Program3.py
+++ Program3.py
@@ -1,3 +1,6 @@
+text = "  Hello, world!  "
+new_text = text.replace(" ", "")
+print(new_text)  # Output: "Helloworld!"
 ## Problem 1 ##
 #Fix the following code so that its output matches
 # Sample output:
@@ -13,24 +16,19 @@
 
 name = "Tim Tester"
 age = 20
-skill1 = "python"
-level1 = "beginner"
-skill2 = "java"
-level2 = "veteran"
-skill3 = "programming"
-level3 = "semiprofessional"
-lower = 2000
-upper = 3000
+skills = [
+    ("python", "beginner"),
+    ("java", "veteran"),
+    ("programming", "semiprofessional"),
+]
+salary_range = (2000, 3000)
 
-print("my name is ", name, " , I am ", age, "years old\n")
+print("my name is", name, ", I am", age, "years old\n")
 print("my skills are")
-print(skill1, " (",level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )\n")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
-
-
-
+for skill, level in skills:
+    print(" -", skill, "(",  level, ")")
+print("\n")
+print("I am looking for a job with a salary of", salary_range[0], "-", salary_range[1], "dollars per month")
 
 
 ## Problem 2 ##
